bcfb1e868556d964794bdbaae2b70f285d3f3e56
yingtung99 <maisy8799@gmail.com>
2026-07-24 16:42:54 +0800

    [fix] 改回本地 apiBaseUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://api.marketday.dev/',
+  apiBaseUrl: 'http://localhost:8081/',
   googleClientId: '322007662830-udt5d156946h171o1edm93bj6pfu16cj.apps.googleusercontent.com',
 };
