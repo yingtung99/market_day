8fc22ef8fd7c3f3b8c4ae994073d48b00d2a464a
simon901125 <83710589+simon901125@users.noreply.github.com>
2026-07-27 21:51:28 +0800

    return

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  //apiBaseUrl: 'http://localhost:8081/',
-  apiBaseUrl: 'https://api.marketday.dev/',
+  apiBaseUrl: 'http://localhost:8081/',
+  //apiBaseUrl: 'https://api.marketday.dev/',
   googleClientId: '322007662830-udt5d156946h171o1edm93bj6pfu16cj.apps.googleusercontent.com',
 };
